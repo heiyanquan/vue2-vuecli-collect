1a657271ac6d6cdf3c8985b8f554389b3da24398
wuy <wuy@hsmap.com>
2022-03-29 17:16:52 +0800

    worker

--- vue.config.js
+++ vue.config.js
@@ -31,12 +31,15 @@ module.exports = {
     config.module
       .rule('worker')
       .test(/\.worker\.js$/)
-      .use('worker-loader')
+      .use('worker')
       .loader('worker-loader')
       .options({
         inline: 'fallback'
       })
       .end()
+    // 解决 "window is undefined", 这是因为 worker 线程中不存在 window 对象, 要用 this 代替: (不过我的项目中配置了这个也不行, 用的 self 代替, 后面介绍)
+    config.output.globalObject('this')
+    config.module.rule('js').exclude.add(/\.worker\.js$/)
   },
   // 解决打包的时报错
   parallel: false,
