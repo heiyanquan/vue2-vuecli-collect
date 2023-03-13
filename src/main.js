df2c21b331c0b59306220b9622e0fe27b9fd8834
heiyanquan <2792652898@qq.com>
2023-03-13 15:00:13 +0800

    up

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import App from './App'
 import store from './store'
 
-Vue.use(VueCompositionAPI)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.config.devtools = true
